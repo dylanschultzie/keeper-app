6d0fa0e08be63aaeed5a6bd0c70376f45a1b4d48
Dylan Schultz <dylanschultz311@gmail.com>
2020-07-12 14:06:45 -0700

    add state to note area

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,11 +1,27 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = React.useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setNote((prevNote) => {
+      console.log(prevNote);
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input onChange={handleChange} value={note.title} name="title" placeholder="Title" />
+        <textarea onChange={handleChange} value={note.content} name="content" placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
