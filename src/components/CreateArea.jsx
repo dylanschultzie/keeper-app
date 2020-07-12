df585acb353f038df92b612f0f33563ba48f3aa8
Dylan Schultz <dylanschultz311@gmail.com>
2020-07-12 14:15:55 -0700

    cleanup add

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -17,26 +17,24 @@ function CreateArea(props) {
     });
   }
 
+  function addNote(event) {
+    props.onAdd(note);
+    setNote((prevNote) => {
+      console.log(prevNote);
+      return {
+        title: "",
+        content: "",
+      };
+    });
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
         <input onChange={handleChange} value={note.title} name="title" placeholder="Title" />
         <textarea onChange={handleChange} value={note.content} name="content" placeholder="Take a note..." rows="3" />
-        <button
-          onClick={(event) => {
-            props.onAdd(note);
-            setNote((prevNote) => {
-              console.log(prevNote);
-              return {
-                title: "",
-                content: "",
-              };
-            });
-            event.preventDefault();
-          }}
-        >
-          Add
-        </button>
+        <button onClick={addNote}>Add</button>
       </form>
     </div>
   );
